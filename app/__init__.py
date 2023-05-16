7f2d1102307f220f320cde270238d9d19c24c2dc
Dmytro2V <op2vin@gmail.com>
2023-05-17 02:58:35 +0300

    up to login form

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from .config import Configuration
+from .models import db, Employee
+from .routes import orders
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+app.register_blueprint(orders.bp)
+db.init_app(app)  # Configure the application with SQLAlchemy
+
+login = LoginManager(app) #  Create the login manager for your application to protect routes.
+login.login_view = "session.login" # Instruct the login manager to use the "session.login" Blueprint function
+
+# Configure the LoginManager to use your load_user function to get Employee objects from the database.
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(int(id))
