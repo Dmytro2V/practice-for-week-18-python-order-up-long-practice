be52a71c36e3aa7c145da69deb1baee367e29456
Dmytro2V <op2vin@gmail.com>
2023-05-17 20:06:32 +0300

    done login-logout, up to menu

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ import os
 class Configuration:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    LOGIN_DISABLED = False # for still working autorization in flask debug mode
